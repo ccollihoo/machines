723765495ba86153a02a48c17449e71406fa9857
Oliver Hoogvliet <oliver.hoogvliet@codecentric.de>
2016-12-16 15:51:40 +0100

    Jenkins container can now use docker in DooD mode.

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -2,6 +2,7 @@
 # https://hub.docker.com/_/jenkins/
 #
 sudo apt-get install docker.io -y
+sudo apt-get autoremove -y
 sudo service docker start
 sudo mkdir -p /var/jenkins
 sudo chmod -R go+w /var/jenkins
