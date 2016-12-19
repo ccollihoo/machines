4fc917f4811a3746fc2952b6f9aa2e745710bf2c
Oliver Hoogvliet <oliver.hoogvliet@codecentric.de>
2016-12-19 11:31:48 +0100

    Regular update for Jenkins container

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -1,6 +1,9 @@
+#!/usr/bin/env bash
 #
 # https://hub.docker.com/_/jenkins/
 #
+sudo apt-get update
+sudo apt-get upgrade -y
 sudo apt-get install docker.io -y
 sudo apt-get autoremove -y
 sudo service docker start
